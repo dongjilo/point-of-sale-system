5efa6dfae844f7032e941c89e2cffe228a3932b1
dongjilo <121302078+dongjilo@users.noreply.github.com>
2023-12-02 01:26:46 +0800

    added bootstrap classes

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,9 +5,13 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.jss"></script>
     <title>Document</title>
 </head>
-<body>
+<body style="background-color: #eee">
+
         {{ $slot }}
+
 </body>
 </html>
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,9 +1,13 @@
 <x-layout>
-    <p>Product Name: {{$product->product_name}}</p>
-    <p>Product Code: {{$product->product_code}}</p>
-    <p>Price: {{$product->product_price}}</p>
-    <p>QTY: {{$product->product_quantity}}</p>
-    <p>Supplier: {{$product->supplier->supplier_name}}</p>
-    <p>Category: {{$product->category->category_name}}</p>
-    <p>User: {{$product->user->user_fname}} {{$product->user->user_lname}}</p>
+    <div class="container mt-5">
+        <div class="card w-50">
+            <p>Product Name: {{$product->product_name}}</p>
+            <p>Product Code: {{$product->product_code}}</p>
+            <p>Price: {{$product->product_price}}</p>
+            <p>QTY: {{$product->product_quantity}}</p>
+            <p>Supplier: {{$product->supplier->supplier_name}}</p>
+            <p>Category: {{$product->category->category_name}}</p>
+            <p>User: {{$product->user->user_name}}</p>
+        </div>
+    </div>
 </x-layout>
