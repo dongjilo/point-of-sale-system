fbe172ad4ca95263dad9caee1ec662824bb297a6
dongjilo <121302078+dongjilo@users.noreply.github.com>
2023-12-15 03:02:27 +0800

    added factories for some models

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,11 +21,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'user_name' => $this->faker->name(),
+            'user_uname' => fake()->userName(),
+            'user_type' => fake()->randomElement(['admin', 'employee']),
+            'user_password' => static::$password ??= Hash::make('password')
         ];
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Supplier;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +16,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(10)->create();
+//        Category::factory(10)->create();
+//        Supplier::factory(10)->create();
+//        Product::factory(10)->create();
     }
 }
