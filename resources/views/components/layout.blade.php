387c53c0aa6f943e57c2065675800fe6de933389
dongjilo <121302078+dongjilo@users.noreply.github.com>
2023-12-15 03:38:34 +0800

    added calculate change

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.jss"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <title>Document</title>
