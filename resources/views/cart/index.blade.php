d9292e32694aefc29b0d2c58f908c0af7d77207d
dongjilo <121302078+dongjilo@users.noreply.github.com>
2023-12-15 03:38:04 +0800

    added bootstrap icons cdn

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -8,7 +8,7 @@
                 <th>Quantity</th>
                 <th>Price</th>
                 <th>Total</th>
-                <th><a href="#" class="btn btn-sm btn-success add">Add</a></th>
+                <th><a href="#" class="btn btn-success add"><i class="bi bi-plus-lg"></i></th>
             </thead>
 
             <tbody class="add-more-product">
@@ -30,17 +30,25 @@
                 </td>
 
                 <td>
-                    <input type="number" class="form-control total" name="total[]" id="total">
+                    <input type="text" class="form-control total" name="total[]" id="total" readonly>
                 </td>
 
                 <td>
-                    <a href="#" class="btn btn-sm btn-danger">Remove</a>
+                    <a href="#" class="btn btn-danger"><i class="bi bi-x-lg"></i></a>
                 </td>
 
             </tbody>
         </table>
 
         <span><h4>Total:</h4><p name="order-total" id="order-total" class="order-total">0.00</p></span>
+
+
+        <span>
+            <label for="amount-paid">Amount Paid</label>
+            <input type="text" name="amount-paid" id="amount-paid" class="form-control w-50 mb-3" placeholder="Amount Paid">
+            <label for="change">Change</label>
+            <input type="text" name="change" id="change" class="form-control w-50" placeholder="Change" readonly>
+        </span>
     </div>
 </div>
 
@@ -56,7 +64,7 @@
                    '<td><input type="number" class="form-control product_quantity" name="product_quantity[]" id="product_quantity"></td>' +
                    '<td><input type="text" class="form-control product_price" name="product_price[]" id="product_price" readonly></td>' +
                    '<td><input type="number" class="form-control total" name="total[]" id="total"></td>' +
-                   '<td><a href="#" class="btn btn-sm btn-danger delete">Remove</a></td></tr>';
+                   '<td><a href="#" class="btn btn-danger delete"><i class="bi bi-x-lg"></i></a></td></tr>';
                $('.add-more-product').append(tr);
                updateRowNumbers();
            });
@@ -67,7 +75,7 @@
            });
 
             function updateRowNumbers() {
-                $('.add-more-product tr').each(function (index, element) {
+                $('.add-more-product tr').each(function (index) {
                     $(this).find('.row-number').html(index + 1);
                 });
                 totalAmount();
@@ -102,6 +110,13 @@
                 var tr = $(this).closest('tr');
                 calculate(tr);
             });
+
+            $('#amount-paid').on('input', function () {
+                var paid = parseFloat($(this).val()) || 0;
+                var total = parseFloat($('.order-total').text()) || 0;
+                var change = (paid - total).toFixed(2);
+                $('#change').val(change >= 0 ? change : 0);
+            })
         });
     </script>
 </x-layout>
