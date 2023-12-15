01f1efd08e6598a03ff8cc1e8efed14c41771d70
dongjilo <121302078+dongjilo@users.noreply.github.com>
2023-12-16 05:19:04 +0800

    minor changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,8 @@ class DatabaseSeeder extends Seeder
     {
 
         User::factory(10)->create();
-//        Category::factory(10)->create();
-//        Supplier::factory(10)->create();
-//        Product::factory(10)->create();
+        Category::factory(10)->create();
+        Supplier::factory(10)->create();
+        Product::factory(10)->create();
     }
 }
