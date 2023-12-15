c79e8f47717f5e90b3fcc243a812e94cc8d7c5f6
dongjilo <121302078+dongjilo@users.noreply.github.com>
2023-12-16 05:21:25 +0800

    added users page

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+
+    public function index() {
+
+        return view('users.index', [
+            'users' => User::all()]);
+    }
+
+    public function show(User $user) {
+        return view('users.show', [
+            'user' =>  $user]);
+    }
+
+    public function edit(User $user) {
+        return view('users.edit', [
+            'user' => $user]);
+    }
+
+    public function update(Request $request, User $user) {
+        $formFields = $request->all();
+        $user->update($formFields);
+        return redirect('users');
+    }
+
+
+
+    public function create() {
+        return view('users.register');
+    }
+
+    public function store(Request $request) {
+        $userInfo = $request->all();
+        $userInfo['user_password'] = bcrypt($userInfo['user_password']);
+        User::create($userInfo);
+        return redirect('/register');
+    }
+
+
+}
