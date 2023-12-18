b8e9b69f354a02c69916fc3f3e54bda852b96c79
ixynGIT <ixynrs@gmail.com>
2023-12-18 09:09:57 +0800

    request rules

---
+++ app/Http/Requests/SupplierSaveRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SupplierSaveRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'supplier_name' => 'required|unique:suppliers',
+            'supplier_phone' => 'required',
+            'supplier_email' =>'required|unique:suppliers',
+        ];
+    }
+
+    public function messages()
+    {
+        return[
+            'supplier_name.unique' => 'Supplier Name has been Taken.',
+            'supplier_phone.required' => 'Supplier Phone is Required!',
+            'supplier_email.unique' => 'Inputed Email has been Taken.',
+        ];
+    }
+}
