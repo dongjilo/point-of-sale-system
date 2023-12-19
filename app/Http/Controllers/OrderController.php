9b7386c53e98867e0ababe1ea1d4092c95786769
dongjilo <121302078+dongjilo@users.noreply.github.com>
2023-12-19 09:59:21 +0800

    bug fix

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -8,6 +8,7 @@ use App\Models\Inventory;
 use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\Product;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -57,67 +58,67 @@ class OrderController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
-            'product_id' => 'required',
-            'product_expiry' => 'required',
-            'product_quantity' => 'required',
-            'product_price' => 'required',
-            'total' => 'required',
-            'payment_method' => 'required',
-            'amount_paid' => 'required',
-            'order_total' => 'required'
-        ]);
-        DB::transaction(function () use ($request) {
-            // order model
-            $order = new Order;
-            $order->user_id = Auth::id();
-            $order->order_date = now();
-            $order->save();
-            $order_id = $order->order_id;
+        try{
+            $request->validate([
+                'product_id' => 'required',
+                'product_expiry' => 'required',
+                'product_quantity' => 'required',
+                'product_price' => 'required',
+                'total' => 'required',
+                'payment_method' => 'required',
+                'amount_paid' => 'required',
+                'order_total' => 'required'
+            ]);
+            DB::transaction(function () use ($request) {
+                // order model
+                $order = new Order;
+                $order->user_id = Auth::id();
+                $order->order_date = now();
+                $order->save();
+                $order_id = $order->order_id;
 
-            // order_items model
-            for ($product_id = 0; $product_id < count($request->product_id); $product_id++) {
-                $order_item = new OrderItem;
-                $order_item->order_id = $order_id;
-                $order_item->product_id = $request->product_id[$product_id];
-                $order_item->order_item_quantity = $request->product_quantity[$product_id];
-                $order_item->order_item_subtotal = $request->total[$product_id];
-                $order_item->save();
+                // order_items model
+                for ($product_id = 0; $product_id < count($request->product_id); $product_id++) {
+                    $order_item = new OrderItem;
+                    $order_item->order_id = $order_id;
+                    $order_item->product_id = $request->product_id[$product_id];
+                    $order_item->order_item_quantity = $request->product_quantity[$product_id];
+                    $order_item->order_item_subtotal = $request->total[$product_id];
+                    $order_item->save();
 
-                $inventory = Inventory::where('product_id', $request->product_id[$product_id])
-                    ->where('inventory_id', $request->inventory_id[$product_id])
-                    ->first();
+                    $inventory = Inventory::where('product_id', $request->product_id[$product_id])
+                        ->where('inventory_id', $request->inventory_id[$product_id])
+                        ->first();
 
-                $inventory->inventory_quantity -= $request->product_quantity[$product_id];
-                $inventory->save();
-            }
+                    $inventory->inventory_quantity -= $request->product_quantity[$product_id];
+                    $inventory->save();
+                }
 
-            foreach ($request->product_id as $key => $productId) {
-                $quantitySold = $request->product_quantity[$key];
-                $totalRevenue = $request->total[$key];
+                foreach ($request->product_id as $key => $productId) {
+                    $quantitySold = $request->product_quantity[$key];
+                    $totalRevenue = $request->total[$key];
 
-                Bestseller::updateOrCreate(
-                    ['product_id' => $productId, 'bestseller_month' => now()->month, 'bestseller_year' => now()->year],
-                    ['bestseller_quantity_sold' => DB::raw("bestseller_quantity_sold + $quantitySold"), 'bestseller_total_sales' => DB::raw("bestseller_total_sales + $totalRevenue")]
-                );
-            }
+                    Bestseller::updateOrCreate(
+                        ['product_id' => $productId, 'bestseller_month' => now()->month, 'bestseller_year' => now()->year],
+                        ['bestseller_quantity_sold' => DB::raw("bestseller_quantity_sold + $quantitySold"), 'bestseller_total_sales' => DB::raw("bestseller_total_sales + $totalRevenue")]
+                    );
+                }
 
-            // billings model
-            $billing = new Billing;
-            $billing->order_id = $order_id;
-            $billing->user_id = Auth::id();
-            $billing->billing_payment_method = $request->payment_method;
-            $billing->billing_total_amount = $request->order_total;
-            $billing->billing_amount_tendered = $request->amount_paid;
-            $billing->billing_date = now();
-            $billing->save();
-
-            // inventory model
-
-            // bestseller model
-        });
-
-        return redirect('/orders_create')->with('success', 'Transaction Successful!');
+                // billings model
+                $billing = new Billing;
+                $billing->order_id = $order_id;
+                $billing->user_id = Auth::id();
+                $billing->billing_payment_method = $request->payment_method;
+                $billing->billing_total_amount = $request->order_total;
+                $billing->billing_amount_tendered = $request->amount_paid;
+                $billing->billing_date = now();
+                $billing->save();
+            });
+            return redirect('/orders_create')->with('success', 'Transaction Successful!');
+        }catch (QueryException $ex){
+            session()->forget('success');
+            return back()->with('error', 'Transaction Failed.');
+        }
     }
 
     public function fetchSalesCount() {
