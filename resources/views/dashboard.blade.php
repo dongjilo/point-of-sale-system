8c5e3b2a538c8926dfb10025a6419970a3eb405b
dongjilo <121302078+dongjilo@users.noreply.github.com>
2023-12-19 21:22:11 +0800

    changed profit to typo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -103,7 +103,7 @@
           <div class="card mb-3">
             <div class="card-header">
               <i class="fa fa-chart-area"></i>
-              Profit this year</div>
+              Sales this year</div>
             <div class="card-body">
               <canvas id="myAreaChart" width="100%" height="30"></canvas>
             </div>
