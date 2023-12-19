d86772b770514c572b0dc62519445a2edd8ff532
dongjilo <121302078+dongjilo@users.noreply.github.com>
2023-12-19 11:04:45 +0800

    downloaded assets

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -170,7 +170,7 @@
         </div>
     </div>
 </div>
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="{{asset('js/jquery-3.7.1.min.js')}}" crossorigin="anonymous"></script>
     <script>
 
         function ani(){
--- resources/views/scaffholding-page.blade.php
+++ resources/views/scaffholding-page.blade.php
@@ -7,29 +7,24 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="Jun-Ianez - Point of Sale System">
   <meta name="author" content="Group 8 - BSIT2D">
-  <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
         crossorigin="anonymous" referrerpolicy="no-referrer"/>
+  <link href="{{asset('css/style.css')}}" rel="stylesheet">
+  <link rel="stylesheet" href="{{asset('css/jquery.dataTables.min.css')}}">
+  <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
 
-  <link href="css/style.css" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('js/angular.min.js')}}"></script>
+  <script src="{{asset('js/jquery-3.7.1.min.js')}}"></script>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-          crossorigin="anonymous"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
-
-  <link rel="stylesheet" href="//cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
-  <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
   <title>@yield('title')</title>
 </head>
 
 <body>
 <nav class="navbar navbar-expand navbar-dark static-top">
-  <a class="navbar-brand mr-1" href="#"><img id="logo" src="images/logo-removebg.png"/>Jun-Ianez</a>
+  <a class="navbar-brand mr-1" href="#"><img id="logo" src="{{asset('images/logo-removebg.png')}}"/>Jun-Ianez</a>
   <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
     <i class="fa fa-bars"></i>
   </button>
@@ -263,9 +258,9 @@
   @include('components.modals')
 
 </div>
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"
-        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-<script src="//cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+
+
+
 <script>
   $(document).ready(function () {
     setTimeout(function () {
@@ -273,11 +268,14 @@
     }, 12000);
   });
 </script>
+
 @yield('script')
-<script src="js/jquery.easing.min.js"></script>
-<script src="js/chart.min.js"></script>
-<script src="js/main.js"></script>
-<script src="js/sidebar.min.js"></script>
-<script src="js/chart-area-demo.js"></script>
+<script src="{{asset('js/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('js/jquery.easing.min.js')}}"></script>
+<script src="{{asset('js/chart.min.js')}}"></script>
+<script src="{{asset('js/main.js')}}"></script>
+<script src="{{asset('js/sidebar.min.js')}}"></script>
+<script src="{{asset('js/chart-area-demo.js')}}"></script>
+
 </body>
 </html>
